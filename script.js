d63e44187780f5d2ed8897e69af9f5ea07427931
ColbyDomingos <colbydomingos@my.whccd.edu>
2024-11-08 12:03:14 -0800

    finished final question

--- script.js
+++ script.js
@@ -46,3 +46,24 @@ console.log(sum(numbers));
 // let arrayValue = [1, 2, 3, 4, 5];
 // reverseArrayInPlace(arrayValue);
 // console.log(arrayValue); // outputs [5, 4, 3, 2, 1]
+let array = [1, 2, 3, 4, 5];
+
+function reverseArray(array){
+    const reversedArray = [];
+    for(let i = array.length - 1; i >= 0; i--){
+        reversedArray.push(array[i]);
+    }
+    return reversedArray;
+}
+
+function reverseArrayInPlace(array){
+    for (let i = 0; i < Math.floor(array.length / 2); i++) {
+        const temp = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = temp;
+    }
+    return array;
+}
+
+console.log(reverseArray(array));
+console.log(reverseArrayInPlace(array));
